9364387d63953d25b0de5f5a5509b087753ba0fb
sudharsan4252 <sudharsan4252@gmail.com>
2024-08-17 17:56:31 +0530

    first commmit

---
+++ src/app/Login/page.tsx
@@ -0,0 +1,12 @@
+import React,{ Fragment, ReactNode } from "react";
+
+const Login:React.FC=()=>{
+return(
+    <Fragment>
+        <div className="container border-pink-500">
+i am login
+        </div>
+    </Fragment>
+)
+}
+export default Login;
---
+++ src/app/dashboard/sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar: React.FC = () => {
+    const list:string[]=[about,home]
+    return (
+        <div className="w-48 h-screen border-green-500 border-2">
+            i am sidebar
+        </div>
+    )
+}
+export default Sidebar;
